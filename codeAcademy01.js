906f76efee7f350bfb7af14791832f6f96716ff3
Whitefoxey <158072517+Whitefoxey@users.noreply.github.com>
2024-02-15 13:01:01 -0600

    Add files via upload

---
+++ codeAcademy01.js
@@ -0,0 +1,91 @@
+//Opening line.
+console.log(5)
+console.log(17)
+
+
+
+
+
+
+
+
+
+
+
+
+console.log('It was love at first sight.');
+
+/*console.log('The first time Yossarian saw the chaplain he fell madly in love with him.');
+console.log('Yossarian was in the hospital with a pain in his lver that fell just short of being jaudice.');
+console.log('The doctors were puzzled by the fact that it wasn\'t quite jaudice.');
+console.log('If it became jaudice they could treat it.');
+console.log('If it didn\'t become jaundice and went away they could discharge him.');
+console.log('But this just being short of jaundice all the time confused them.');
+*/
+
+
+
+
+
+console.log('Location of Codecademy headquaters: 575 Broadway, New York City');
+console.log(40);
+
+console.log('JavaScript');
+console.log(2011);
+console.log('Woohoo! I love to code! #codecademy');
+console.log(20.49);
+
+
+
+
+
+console.log(3 + 4); // 7
+console.log(5 - 1); // 4
+console.log(4*2);   // 8
+console.log(9/3);   // 3
+
+console.log(11%3);   // 2
+console.log(12%3);  // 0
+
+console.log(3.5 + 19);
+console.log(2023 - 1969);
+console.log(65/240);
+console.log(.2708 * 100);
+
+
+console.log('hi' + 'ya');
+console.log('wo' + 'ah');
+console.log('I love to ' + 'code.');
+
+console.log('front ' + 'space');
+console.log('back' + ' space');
+console.log('no' + 'space');
+console.log('middle' + ' ' + 'space');
+
+console.log('One' + ', ' + 'two' + ', ' + 'three!');
+
+console.log('Hello' + 'World');
+console.log('Hello' + ' ' + 'World');
+
+console.log('Hello'.length);    // prints 5
+
+console.log('Teaching the world how to code'.length);   // prints 30
+
+console.log('hello'.toUpperCase()); //Prints 'HELLO'
+console.log('Hey'.startsWith('H')); //Prints true
+
+console.log('Codecademy'.toUpperCase());
+console.log(' Remove whitespace '.trim());
+
+console.log(Math.random());
+
+Math.random * 50;
+
+Math.floor(Math.random() * 50);
+
+console.log(Math.floor(Math.random() * 50));
+
+console.log(Math.random() * 100);
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
